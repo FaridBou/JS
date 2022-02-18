717b5975aa3143dc09f31b06493958766ef801cf
Ruben <navone_ruben@msn.com>
2022-02-18 14:38:37 +0100

    correction

--- eval24/eval24.js
+++ eval24/eval24.js
@@ -11,21 +11,28 @@ let randNumb = Math.floor(Math.random()*1000);
 buttonSelector.addEventListener("click", verification);
 
 function rappel() {
-    affichage = Number(inputUserSelector.value);
+    affichage = inputUserSelector.value;  //pas la peine de convertir à ce moment
     if (compteur === 1) {
         inputUserSelector.textContent = 'Choix précédent: ';
     }
     inputUserSelector.textContent += inputUser + ' ';
 }
+
 function verification(){
     let inputUser = inputUserSelector.value;
-        if(inputUser < randNumb){
-            affichage.textContent = "C'est plus !";
-        }else if (inputUser > randNumb) {
-            affichage.textContent = "C'est moins";
-        }else {
-            affichage.textContent = "Bravo c'est gagné";
+        //Tu n'a pas fait la gestion des erreur     
+    if(inputUser < 0 || inputUser > 1000 || isNaN(inputUser) || inputUser === ""){ //si plus petit que 0 ou plus grand que 1000 ou pas un nombre ou vide
+        affichage.textContent = "Saisis incorrecte !";  //Affiche une erreur
+        }else{                                          //sinon continue le code
+            if(inputUser < randNumb){
+                affichage.textContent = "C'est plus !";
+            }else if (inputUser > randNumb) {
+                affichage.textContent = "C'est moins";
+            }else {
+                affichage.textContent = "Bravo c'est gagné";
+            }
         }
+    
     inputUserSelector.focus();
     compteur++;
 }
